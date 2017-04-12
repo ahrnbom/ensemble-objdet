0bf302b6febf6e4d6d8b7c7f2950c3d441cfb661
Martin Arhnbom <ma@maths.lth.se>
2017-04-12 11:06:38 +0200

    Added support for weights

--- ensemble.py
+++ ensemble.py
@@ -3,11 +3,12 @@ Ensembling methods for object detection.
 """
 
 """ 
-Basic Ensemble - find overlapping boxes of the same class and average their positions
-while adding their confidences
+General Ensemble - find overlapping boxes of the same class and average their positions
+while adding their confidences. Can weigh different detectors with different weights.
+No real learning here, although the weights and iou_thresh can be optimized.
 
 Input: 
- - dets : List of detections. Each detection is the output from one detector, and
+ - dets : List of detections. Each detection is all the output from one detector, and
           should be a list of boxes, where each box should be on the format 
           [box_x, box_y, box_w, box_h, class, confidence] where box_x and box_y 
           are the center coordinates, box_w and box_h are width and height resp.
@@ -15,26 +16,49 @@ Input:
 
  - iou_thresh: Threshold in terms of IOU where two boxes are considered the same, 
                if they also belong to the same class.
+               
+ - weights: A list of weights, describing how much more some detectors should
+            be trusted compared to others. The list should be as long as the
+            number of detections. If this is set to None, then all detectors
+            will be considered equally reliable. The sum of weights does not
+            necessarily have to be 1.
 
 Output:
     A list of boxes, on the same format as the input. Confidences are in range 0-1.
 """
-def BasicEnsemble(dets, iou_thresh = 0.5):
-    ndets = len(dets)
-    out = list()
-
-    used = dict()
+def GeneralEnsemble(dets, iou_thresh = 0.5, weights=None):
+    assert(type(iou_thresh) == float)
     
-    for det in dets:
-        other_dets = [d for d in dets if d != det] 
+    ndets = len(dets)
+    
+    if weights is None:
+        w = 1/float(ndets)
+        weights = [w]*ndets
+    else:
+        assert(len(weights) == ndets)
+        
+        s = sum(weights)
+        for i in range(0, len(weights)):
+            weights[i] /= s
+
+    out = list()
+    used = list()
+    
+    for idet in range(0,ndets):
+        det = dets[idet]
         for box in det:
             if box in used:
                 continue
                 
-            used[box] = 1
+            used.append(box)
             # Search the other detectors for overlapping box of same class
             found = []
-            for odet in other_dets:
+            for iodet in range(0, ndets):
+                odet = dets[iodet]
+                
+                if odet == det:
+                    continue
+                
                 bestbox = None
                 bestiou = iou_thresh
                 for obox in odet:
@@ -46,10 +70,11 @@ def BasicEnsemble(dets, iou_thresh = 0.5):
                             if iou > bestiou:
                                 bestiou = iou
                                 bestbox = obox
-                
+                                
                 if not bestbox is None:
-                    found.append(bestbox)
-                    used[bestbox] = 1
+                    w = weights[iodet]
+                    found.append((bestbox,w))
+                    used.append(bestbox)
                             
             # Now we've gone through all other detectors
             if len(found) == 0:
@@ -57,7 +82,7 @@ def BasicEnsemble(dets, iou_thresh = 0.5):
                 new_box[5] /= ndets
                 out.append(new_box)
             else:
-                allboxes = [box]
+                allboxes = [(box, weights[idet])]
                 allboxes.extend(found)
                 
                 xc = 0.0
@@ -66,28 +91,32 @@ def BasicEnsemble(dets, iou_thresh = 0.5):
                 bh = 0.0
                 conf = 0.0
                 
-                for b in allboxes:
-                    xc += b[0]
-                    yc += b[1]
-                    bw += b[2]
-                    bh += b[3]
-                    conf += b[5]
-                    
-                nall = len(allboxes)
-                xc /= nall
-                yc /= nall
-                bw /= nall
-                bh /= nall
-                conf /= ndets
+                wsum = 0.0
+                for bb in allboxes:
+                    w = bb[1]
+                    wsum += w
+
+                    b = bb[0]
+                    xc += w*b[0]
+                    yc += w*b[1]
+                    bw += w*b[2]
+                    bh += w*b[3]
+                    conf += w*b[5]
+                
+                xc /= wsum
+                yc /= wsum
+                bw /= wsum
+                bh /= wsum    
+
                 new_box = [xc, yc, bw, bh, box[4], conf]
                 out.append(new_box)
     return out
     
 def getCoords(box):
-    x1 = box[0] - box[2]/2
-    x2 = box[0] + box[2]/2
-    y1 = box[1] - box[3]/2
-    y2 = box[1] + box[3]/2
+    x1 = float(box[0]) - float(box[2])/2
+    x2 = float(box[0]) + float(box[2])/2
+    y1 = float(box[1]) - float(box[3])/2
+    y2 = float(box[1]) + float(box[3])/2
     return x1, x2, y1, y2
     
 def computeIOU(box1, box2):
@@ -108,3 +137,14 @@ def computeIOU(box1, box2):
     
     iou = intersect_area / (box1_area + box2_area - intersect_area)
     return iou
+    
+if __name__=="__main__":
+    # Toy example
+    dets = [ 
+            [[0.1, 0.1, 1.0, 1.0, 0, 0.9], [1.2, 1.4, 0.5, 1.5, 0, 0.9]],
+            [[0.2, 0.1, 0.9, 1.1, 0, 0.8]],
+            [[5.0,5.0,1.0,1.0,0,0.5]]
+           ]
+    
+    ens = GeneralEnsemble(dets, weights = [1.0, 0.1, 0.5])
+    print(ens)
